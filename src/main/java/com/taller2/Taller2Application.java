a4b9212b136174436718a4dfee3a80e4f23b5895
facu31 <facujavaw@gmail.com>
2022-08-07 12:57:10 -0300

    Se agrega menu

--- src/main/java/com/taller2/Taller2Application.java
+++ src/main/java/com/taller2/Taller2Application.java
@@ -5,12 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class Taller2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Taller2Application.class, args); 
+		
+
+		 
 	}
     
 }
